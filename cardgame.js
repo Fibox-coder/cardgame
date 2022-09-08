2f9bdb677a008a1780a79d3404a41c0c3f63a99f
Tim-D- <Timvandeutekom@hotmail.com>
2022-09-08 15:03:35 +0200

    Adds a third card to the game

--- cardgame.js
+++ cardgame.js
@@ -1,21 +1,28 @@
-const card1 = document.getElementById("card1")
-const card2 = document.getElementById("Lara")
 const attackButton = document.querySelector('#attack-button');
 
 
-const cardMage = {name:"Mage", attack: 3 , health: 5, card1};
-const cardLara = {name:"Lara", attack: 5 , health: 8, card2};
+
+const mage = document.getElementById("mage")
+const lara = document.getElementById("lara")
+const jeanne = document.getElementById("jeanne")
+
+
+const cardMage = {name:"Mage", attack: 3 , health: 5, mage};
+const cardLara = {name:"Lara", attack: 5 , health: 9, lara};
+const cardJeanne = {name:"Jeanne", attack: 1 , health: 10, jeanne};
 
 document.getElementById("attackDisplayMage").innerText = cardMage.attack;
 document.getElementById("attackDisplayLara").innerText = cardLara.attack;
+document.getElementById("attackDisplayJeanne").innerText = cardJeanne.attack;
 
 document.getElementById("healthDisplayMage").innerText = cardMage.health;
 document.getElementById("healthDisplayLara").innerText = cardLara.health;
+document.getElementById("healthDisplayJeanne").innerText = cardJeanne.health;
 
 function playGame(){
   let healthLara = cardLara.health
   if (healthLara > 0){
-    let newHealthLara = healthLara - cardMage.attack
+    let newHealthLara = healthLara - cardMage.attack - cardJeanne.attack
   cardLara.health = newHealthLara
   } else {
     alert("u dead bruh...")
